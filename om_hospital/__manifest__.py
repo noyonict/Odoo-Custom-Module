9ec5f8a16aa69be92260d937d0a1a34f5411bba9
noyon <noyonict@gmail.com>
2020-06-04 17:06:26 +0600

    added report for pataint

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -12,6 +12,8 @@
         'data/sequence.xml',
         'views/patient.xml',
         'views/appointment.xml',
+        'reports/report.xml',
+        'reports/patient_card.xml',
     ],
     'demo': [],
     'installable': True,
