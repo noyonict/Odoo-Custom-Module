7a57917aa05d0018bca7b3d66f273eecd9c70ea7
Noyon <denary.noyon@gmail.com>
2020-06-16 12:55:07 +0600

    get data from db in odoo

--- om_hospital/wizards/create_appointment.py
+++ om_hospital/wizards/create_appointment.py
@@ -17,3 +17,10 @@ class CreatAppointment(models.TransientModel):
         self.patient_id.message_post(body='Appointment Created Successfully', subject='Appointment Creation')
         self.env['hospital.appointment'].create(vals)
 
+    def get_data(self):
+        print('Get data function worked!')
+        # appointments = self.env['hospital.appointment'].search(['patient_id', '=', 2])
+        appointments = self.env['hospital.appointment'].search([])
+        for appointment in appointments:
+            print('Appointment ID:', appointment.name)
+
